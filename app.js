32dcf6e6f810221168a4fac32eeb91d50f1a5ca0
otolis <lagonikasa@gmail.com>
2025-06-03 15:46:55 +0300

    added memmory to the messages

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const path = require("path");
-const indexRouter = require("./routes/index");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+const messages = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { messages });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  const { username, message } = req.body;
+  if (username && message) {
+    messages.push({
+      username,
+      message,
+      time: new Date().toLocaleString(),
+    });
+  }
+  res.redirect("/");
+});
 
 app.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+  console.log("Server running at http://localhost:3000");
 });
